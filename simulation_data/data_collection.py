2c8a24b8bfe92c8930d3c4e75315b7a5d5bb2ce5
joshualynch42 <73693363+joshualynch42@users.noreply.github.com>
2022-02-10 14:23:59 +0000

    amending coordinate data

--- simulation_data/data_collection.py
+++ simulation_data/data_collection.py
@@ -5,10 +5,16 @@ from vsp.processor import CameraStreamProcessor, AsyncProcessor
 import matplotlib.pylab as plt # needs to be after initialising controller (strange bug)
 import pandas as pd
 import string
+import time
 
-alphabet_list = list(string.ascii_lowercase)
-print(alphabet_list)
-key_coords = pd.read_csv('D:\Josh\github\individual_project\simulation_data\key_coords.csv')
+alphabet_list = list(string.ascii_uppercase)
+key_coords = pd.read_csv(r"D:\Users\Josh\github\individual_project\simulation_data\key_coords.csv")
+
+line1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+line2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+line3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+line4 = ['SPACE', 'LEFT', 'UP', 'DOWN', 'RIGHT']
+
 
 def make_sensor(): # amcap: reset all settings; autoexposure off; saturdation max
     camera = CvPreprocVideoCamera(source=1,
@@ -24,24 +30,28 @@ def make_sensor(): # amcap: reset all settings; autoexposure off; saturdation ma
 
 # Move robot and collect data
 with SyncRobot(Controller()) as robot, make_sensor() as sensor:
-    robot.linear_speed = 10
+    robot.linear_speed = 40
     robot.coord_frame = [0, 0, 0, 0, 0, 0] # careful
 
+    for current_letter in line4:
     #for current_letter in alphabet_list:
-
-    # get coords for current letter
-    current_letter = alphabet_list[0] #temp
-    x, y = df.loc[current_letter]['X'], df.loc[current_letter]['Y']
-
-    robot.move_linear([x, y, 0, 0, 0, 0])
-    frames_sync = sensor.process(num_frames=5, start_frame=1, outfile=r'C:/Temp/frames_sync.png')
+        # current_letter = alphabet_list[i] #temp
+        x, y = key_coords.loc[key_coords['Key'] == current_letter]['X'], key_coords.loc[key_coords['Key'] == current_letter]['Y']
+        print('current letter is {}'.format(current_letter))
+        robot.move_linear([x, y, -10, 0, 0, 0]) #move horizontal
+        robot.move_linear([x, y, -30, 0, 0, 0]) #move vertical - down
+        frames_sync = sensor.process(num_frames=5, start_frame=1, outfile=r'C:/Temp/frames_sync.png')
+        robot.move_linear([x, y, -10, 0, 0, 0]) #move vertical - up
+        time.sleep(1)
     #frames_sync = sensor.process(num_frames=5, start_frame=1, outfile=r'D:\Josh\github\individual_project\simulation_data\{}_frames.png'.format(current_letter))
 
+    #set robot to a Home
+    robot.move_linear([168, -80, 0, 0, 0, 0])
 # display results
-import matplotlib.pylab as plt # needs to be after initialising controller (strange bug)
-for i, frame in enumerate(frames_sync):
-    plt.title(f'Frame {i+1}')
-    plt.imshow(frame, cmap="Greys")
-    plt.xticks([]); plt.yticks([]);
-    plt.pause(0.1)
-plt.show()
+# import matplotlib.pylab as plt # needs to be after initialising controller (strange bug)
+# for i, frame in enumerate(frames_sync):
+#     plt.title(f'Frame {i+1}')
+#     plt.imshow(frame, cmap="Greys")
+#     plt.xticks([]); plt.yticks([]);
+#     plt.pause(0.1)
+# plt.show()
