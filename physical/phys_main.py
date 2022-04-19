fa0a136823f8c679c830194daf3e11ef81a17422
joshualynch42 <73693363+joshualynch42@users.noreply.github.com>
2022-04-19 10:23:05 +0100

    update

--- physical/phys_main.py
+++ physical/phys_main.py
@@ -1,7 +1,8 @@
 import sys
-sys.path.insert(1, 'D:/Josh\github/individual_project/simulation')
+sys.path.insert(1, 'D:/Users/Josh/github/individual_project/simulation')
 from dueling_ddqn_per import *
 from phys_utils import *
+import time
 
 def make_sensor(): # amcap: reset all settings; autoexposure off; saturdation max
     camera = CvPreprocVideoCamera(source=0,  # might need changing for webcam
@@ -35,9 +36,22 @@ rl_params = {
 
 avg_rew_size = 1
 
+# # TIMING ##
+# total_time_dict = {}
+# time_per_ep_dict = {}
+# time_per_step_dict = {}
+# time_to_converge_dict = {}
+# acc_300_dict = {}
+# accuracy_point = 3000
+
 env = phys_discrete_alphabet_env()
 agent = Dueling_Per_DDQNAgent(env, rl_params)
 agent.load_model('alphabet_Dueling Double Per')
+
+
+start_time = time.time()
+converge_time = time.time()
+converge_bool = False
 
 reward_arr = []
 episodes = rl_params['episodes']
@@ -67,10 +81,26 @@ for episode in range(episodes):
 
     reward_arr.append(reward_tot)
 
+    # ### CHECK CONVERGE ### 95%
+    # if len(reward_arr) > 20 and converge_bool == False:
+    #     mean = sum(reward_arr[episode-20: episode]) / 20
+    #     if mean >= 0.95:
+    #         converge_bool = True
+    #         converge_time = time.time()
+
     print('episode: {}, reward: {}, epsilon: {}, steps: {}, start letter: {}, '
                                 'goal_letter: {}, end_letter: {}, success: {}, done: {}'.
     format(episode, reward_tot, round(agent.epsilon, 3), steps, env.starting_letter,
                 env.goal_letter, coords_to_letter(env.current_coords), success, done))
+
+# ### TIMINGS ###
+# end_time = time.time()
+# time_elapsed = end_time - start_time
+# total_time_dict[agent.label] = time_elapsed
+# time_per_ep_dict[agent.label] = time_elapsed/episodes
+# time_per_step_dict[agent.label] = time_elapsed/total_steps
+# time_to_converge_dict[agent.label] = converge_time - start_time
+# acc_300_dict[agent.label] = sum(reward_arr[accuracy_point:-1]) / (episodes - accuracy_point)
 
 # calculating moving average of reward array
 avg_reward_arr = [] # calcualting moving average
@@ -81,6 +111,13 @@ for i in range(len(reward_arr) - avg_rew_size +1):
 
 x = np.linspace(avg_rew_size, episodes, episodes-avg_rew_size+1)
 plt.plot(x, avg_reward_arr, label=agent.label)
+
+# ### PRINT TIMINGS ###
+# print('Total time ', total_time_dict)
+# print('Time per episode ', time_per_ep_dict)
+# print('Time per step ', time_per_step_dict)
+# print('Time to converge ', time_to_converge_dict)
+# print('Accuracy ', acc_300_dict)
 
 plt.xlabel('episode')
 plt.ylabel('reward')
